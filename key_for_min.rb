00274d167c3bad1d958abab16b4f75156f495650
tracey brimage <onelovenilynn@gmail.com>
2019-10-01 16:35:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,6 @@
-hash = {:blake => 500, :ashley => 2, :adam => 1}
+hash = {blake: 500, ashley : 2, adam :1}
 
 def key_for_min_value(hash)
-
+hash = {blake: 500, ashley : 2, adam :1}
+hash.each do |
 end
